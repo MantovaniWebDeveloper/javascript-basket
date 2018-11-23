33533fda854f093c1b5e8c143f59c455a9409f88
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2018-11-23 16:31:19 +0100

    Inserito trova codice

--- js/main.js
+++ js/main.js
@@ -5,16 +5,13 @@ function numeroRandom(numeroMin, numeroMax){
 }
 
 //funzion generatore Codice
-var arrayLettere = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","u","v","w","x","y"];
-var arrayNumeri = [0,1,2,3,4,5,6,7,8,9,10];
-
-
 function generaCodice(arrayNumeri,arrayLettere){
 
   var codiceToString = "";
   var arrayCodice = [];
   var arrayLettereLunghezza = arrayLettere.length - 1;
   var arrayNumeriLunghezza = arrayNumeri.length - 1;
+
   for (var i = 0; i < 3; i++) {
     var mischiaArray = (Math.floor((Math.random()*arrayLettereLunghezza)+1));
     arrayCodice.push(arrayLettere[mischiaArray])
@@ -27,15 +24,17 @@ function generaCodice(arrayNumeri,arrayLettere){
 
   return codiceToString;
 }
-
-var codice = generaCodice(arrayNumeri,arrayLettere);
-console.log(codice);
 //genero aun array di oggeti con 100 oggetti giocatori basket
+var arrayLettere = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","u","v","w","x","y"];
+var arrayNumeri = [0,1,2,3,4,5,6,7,8,9];
 var legaBasket = [];
 for (var i = 0; i <= 100; i++) {
   nuovoGiocatore = {
     //ad ogni giocatore di basket vanno assciate
     //queste propietà
+    //Codice giocatore univoco(formato da 3 lettere maiuscole
+    //casuali e tre numeri)
+    "codiceUnivoco" : generaCodice(arrayNumeri,arrayLettere),
     "puntiRealizzati" : numeroRandom(1,50),
     "rimbalzi" : numeroRandom(1,25),
     "falli" : numeroRandom(1,10)
@@ -44,5 +43,23 @@ for (var i = 0; i <= 100; i++) {
 }
 
 console.log(legaBasket);
-//Codice giocatore univoco(formato da 3 lettere maiuscole
-//casuali e tre numeri)
+
+//TROVA CODICE
+var trovato = false;
+var i = 0;
+var codiceDaUtente = prompt("inserisci codice di ricerca");
+
+while (trovato == true || i < legaBasket.length) {
+  codiceSalvato = legaBasket[i].codiceUnivoco;
+  if (codiceSalvato == codiceDaUtente) {
+    console.log("Punti realizzati : " + legaBasket[i].puntiRealizzati);
+    console.log("Rimbalzi : " +legaBasket[i].rimbalzi);
+    console.log("Falli : " +legaBasket[i].falli);
+    trovato = true;
+  }
+  else {
+    console.log("codice insesitente");
+    trovato = false;
+    i++;
+  }
+}
