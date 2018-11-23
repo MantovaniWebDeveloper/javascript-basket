9a6a5756534338bdb6799e908265a0e6085cf536
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2018-11-23 14:19:03 +0100

    init progetto e inizio stesura della logica scritta in italiano

---
+++ js/main.js
@@ -0,0 +1,7 @@
+//genero 100 oggetti giocatori basket
+
+//ad ogni giocatore di basket vanno assciate
+//queste propietà
+
+//Codice giocatore univoco(formato da 3 lettere maiuscole
+//casuali e tre numeri)
